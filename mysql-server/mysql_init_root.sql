df9a19911c1c225f0792491f7226e81151737a0e
Jakub Porebski <jakub_porebski@epam.com>
2023-11-22 13:19:53 +0100

    MySQL Server docker-compose

--- mysql-server/mysql_init_root.sql
+++ mysql-server/mysql_init_root.sql
@@ -1,7 +1,7 @@
 
 create user root@'%' identified by 'root';
 grant all on *.* to root@'%' with grant option;
+
+drop user root@'localhost';
+
 flush privileges;
-
-
-
--- simple-stress/stresik.py
+++ simple-stress/stresik.py
@@ -9,7 +9,7 @@ for i in range(5000):
     m = hashlib.sha512()
     m.update(secrets.token_bytes(1024 * 1024))
     s = m.digest()
-    if len(a) > 0:
+    if len(a) > 3:
         a.pop(0)
     a.append(s)
 
